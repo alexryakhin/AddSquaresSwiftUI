dd8ddc34caab8ae53467a107c033aa7b68d54dcd
Alexander Ryakhin <a.ryahin@trinitydigital.ru>
2022-01-31 13:46:46 +0300

    Initial Commit

---
+++ AddSquaresSwiftUI/AddSquaresSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddSquaresSwiftUIApp.swift
+//  AddSquaresSwiftUI
+//
+//  Created by Alexander Ryakhin on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct AddSquaresSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AddSquaresSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddSquaresSwiftUI
+//
+//  Created by Alexander Ryakhin on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
